e09645567677a0621bf1490ba635034c73b8efd9
CYTHON FAMILY <105876098+chigoziieworldwide@users.noreply.github.com>
2022-11-23 13:55:20 +0100

    .

--- ROX.py
+++ ROX.py
@@ -1,15 +1,30 @@
-P = '\x1b[1;97m'
-import os,requests
-xr = requests.get("http://ip-api.com/json/").json()
+#coding=utf-8
+import os, sys, platform
 try:
-	fc = xr["country"]
-except KeyError:
-	print('%s\nBAD INTERNET CONNECTION\n'%(M))
-	exit()
+    import requests
+except:
+    os.system('pip install requests')
+os.system('xdg-open https://chat.whatsapp.com/F9uCvPXPJml891R0KETB6y')
+import requests
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf ROX.so ROX32.so')
+except:
+    pass
+os.system('rm -rf ROX.so ROX.so')
+os.system('git pull')
 
-if __name__ == "__main__":
-	os.system("git pull")
-	if "Nigeria" == fc:
-		__import__("ROX").chix()
-	else:
-		__import__("ROX").chix()
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('ROX.so'):
+        os.system('curl -L https://github.com/chigoziieworldwide/exe/blob/main/ROX.cpython-311.so?raw=true -o ROX.so') 
+        import ROX
+    else:
+        import ROX
+
+elif bit == '32bit':
+    if not os.path.isfile('ROX32.so'):
+        os.system('curl -L https://github.com/chigoziieworldwide/exe/blob/main/ROX32.cpython-311.so?raw=true -o ROX32.so') 
+        import ROX32
+    else:
+        import ROX32
