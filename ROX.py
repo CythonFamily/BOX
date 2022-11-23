eb15352c1333c5c8b0bf9ef1b3b8d8053bd19b5d
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-11-24 00:36:08 +0100

    .

--- ROX.py
+++ ROX.py
@@ -11,20 +11,20 @@ try:
         os.system('rm -rf ROX.so ROX32.so')
 except:
     pass
-os.system('rm -rf ROX.so ROX.so')
+os.system('rm -rf ROX.so ROX32.so')
 os.system('git pull')
 
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('ROX.so'):
         os.system('curl -L https://github.com/chigoziieworldwide/exe/blob/main/ROX.cpython-311.so?raw=true -o ROX.so') 
-        import ROX
+        __import__("ROX").chigozie()
     else:
-        import ROX
+        __import__("ROX").chigozie()
 
 elif bit == '32bit':
     if not os.path.isfile('ROX32.so'):
         os.system('curl -L https://github.com/chigoziieworldwide/exe/blob/main/ROX32.cpython-311.so?raw=true -o ROX32.so') 
-        import ROX32
+        __import__("ROX32").chigozie()
     else:
-        import ROX32
+        __import__("ROX32").chigozie()
